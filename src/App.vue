48ca20736d4b4c5dd7d121034967d2e4a5f70742
NewBoy <1204992207@qq.com>
2019-06-14 14:35:31 +0800

    inti fitst Vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuider v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuider from './components/FooterGuide/FooterGuider'
+
+  export default {
+    components: {
+      FooterGuider
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    color: read
+</style>
